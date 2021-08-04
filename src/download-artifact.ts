5f375242b838481e43f234f9f28ba8a0018d8943
Mingzi <yimysty@github.com>
2021-08-04 14:49:38 -0700

    Update download-artifact.ts

--- src/download-artifact.ts
+++ src/download-artifact.ts
@@ -41,6 +41,8 @@ async function run(): Promise<void> {
       const downloadOptions = {
         createArtifactFolder: false
       }
+      core.info(`${process.env['ACTIONS_RUNTIME_URL']} run time url`)
+      core.info(`it's a new log type`)
       const downloadResponse = await artifactClient.downloadArtifact(
         name,
         resolvedPath,
